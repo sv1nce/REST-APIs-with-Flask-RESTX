a4314bccd4d4a95deac75d383df5b473a1a8fe70
sv1nce <sviatoslav.kushnir.work@gmail.com>
2023-10-23 14:15:12 +0300

    initialization of an API, first start

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 
+from .extensions import api, db
+
 def create_app():
     app = Flask(__name__)
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    
+    api.init_app(app)
+    db.init_app(app)
     
     return app
     
--- app/extensions.py
+++ app/extensions.py
@@ -2,4 +2,4 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restx import Api
 
 api = Api()
-db = SQLAlchemy
+db = SQLAlchemy()
